80a8984bd8f4b236546d011a0cbe9ac0ddc7f964
Javier <javier@eskura21.com>
2021-04-06 20:31:28 +0200

    Pipes e internacionalización

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   titulo = 'Ejemplos básicos';
   imgSrc = 'assets/imgs/ejemplo.jpg';
+  fecha = new Date();
+  objeto = { id: 5, nombre: 'Javier', fecha: new Date() };
+  array = [1, 3, 5, 7, 9];
+
+  mapeoMensajes:
+    { [k: string]: string } = {
+      '=0': 'No tienes ningún mensaje.',
+      '=1': 'Un mensaje.',
+      other: '# mensajes.'
+    };
+
+  diccionario = { casa: 'home', perro: 'dog', other: 'No reconocido' };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TituloComponent } from './titulo.component';
+
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
