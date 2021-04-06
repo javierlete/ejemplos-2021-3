6c7d1e0d13232fa4c5a5dd865694724e7394f5c8
Javier <javier@eskura21.com>
2021-04-06 19:34:12 +0200

    Interpolación, propiedad y assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejemplos';
+  titulo = 'Ejemplos básicos';
+  imgSrc = 'assets/imgs/ejemplo.jpg';
 }
